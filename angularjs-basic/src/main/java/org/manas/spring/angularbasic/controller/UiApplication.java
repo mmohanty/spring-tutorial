449fe4efd12c864389bb6d6c2b14abe853893e17
Manas Mohanty <manasmohanty@Manass-MacBook-Pro.local>
2017-10-19 09:04:28 +0530

    deleting src folder

---
+++ angularjs-basic/src/main/java/org/manas/spring/angularbasic/controller/UiApplication.java
@@ -0,0 +1,20 @@
+package org.manas.spring.angularbasic.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class UiApplication {
+
+    @RequestMapping("/resource")
+    public Map<String,Object> home() {
+        Map<String,Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+}
